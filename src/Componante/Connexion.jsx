8802db013a8ce5071b9eaec0d364c0fab91a5582
mattheo <ansseaumattheo1@gmail.com>
2025-09-11 10:12:37 +0200

    Commit du router dom et page de connexion

---
+++ src/Componante/Connexion.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Connexion() {
+    return (
+        <div>
+            <label>Connexion</label>
+        </div>
+    )
+
+}
+
+export default Connexion;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import Connexion from "./Componante/Connexion.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+    <BrowserRouter>
+        <Connexion />
+    </BrowserRouter>,
+);
