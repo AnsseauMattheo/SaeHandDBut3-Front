7f6b6feaf33c41d269fe2d2eefc8ed56da5e669e
mattheo <ansseaumattheo1@gmail.com>
2025-09-18 10:21:59 +0200

    Image du terrain vide sur Stat Tir

--- src/Pages/StatTir.jsx
+++ src/Pages/StatTir.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import CarteTirs from "../components/CarteTirs.jsx";
 
 export default function StatTir() {
     return (
         <>
             <label>Test StatTir</label>
-            {/*<img*/}
-            {/*    src="https://placekitten.com/800/400"*/}
-            {/*/>*/}
+            <CarteTirs/>
         </>
     )
 }
