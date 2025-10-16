cdcb19ac678061fd3af6bbe8c234285db2461f52
sylle <syllektis59@gmail.com>
2025-10-16 09:28:54 +0200

    fix de la box et lisibilité

--- src/components/StatCard/LastMatchCard.jsx
+++ src/components/StatCard/LastMatchCard.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import StatCard from "./StatCard";
 
-export default function LastMatchCard({match}){
+export default function LastMatchCard({match}) {
     const date = new Date(match.date).toLocaleDateString("fr-FR", {
         day: "2-digit",
         month: "long",
         year: "numeric",
     });
+
     return (
         <StatCard
-        className={`max-w-1/5  border-2 ${
-            match.win ? "border-green-500" : "border-red-500"
-        }`}
+            className={`max-w-1/5 border-2 ${
+                match.win ? "border-green-500" : "border-red-500"
+            }`}
         >
-            <h3 className="text-gray-500 text-center font-medium">Dernier Match</h3>
-            <p className="text-gray-400 text-center">{date}</p>
-            <p className="text-2xl text-center font-bold text-gray-900">{match.adversaire}</p>
-            <p className="mt-2 text-lg text-center font-semibold">
-                Score : {match.butsMis} - {match.butsEncaisses}
-            </p>
+            <h3 className="text-gray-500 text-center text-sm font-medium">Dernier Match</h3>
+            <p className="text-gray-400 text-center text-xs mb-3">{date}</p>
 
+            <div className="grid grid-cols-3 items-start gap-4 p-2">
+                <div className="flex flex-col items-center text-base font-bold">
+                    <span className="text-center">SAHB</span>
+                    <span className="mt-2">{match.butsMis}</span>
+                </div>
+
+                <div className="flex justify-center text-2xl font-bold text-gray-400">
+                    -
+                </div>
+
+                <div className="flex flex-col items-center text-base font-bold">
+                    <span className="text-center">{match.adversaire}</span>
+                    <span className="mt-2">{match.butsEncaisses}</span>
+                </div>
+            </div>
         </StatCard>
     )
 }
