4a973c5c70e64d956fe001e8409b946497422b7c
trykraj <Trystan.Krajewski@uphf.fr>
2025-10-20 14:44:13 +0200

    ajout graph joueuse

--- src/Pages/ProfilJoueuse.jsx
+++ src/Pages/ProfilJoueuse.jsx
@@ -3,11 +3,13 @@ import { Car } from "lucide-react";
 import { use, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import CarteJoueuse from "../components/CarteJoueuse";
+import GraphStats from "../components/GraphStats";
 
 const ProfilJoueuse = () => {
 
     const { id } = useParams();
     const [joueuse, setJoueuse] = useState(null);
+    const [datas, setDatas] = useState(null);
 
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_SERVER_URL}/joueuses/joueuse/${id}`, { withCredentials: true })
@@ -18,18 +20,55 @@ const ProfilJoueuse = () => {
             .catch((err) => {
                 console.error("Erreur lors de la récupération du profil :", err);
             });
+
+        axios.get(`${import.meta.env.VITE_SERVER_URL}/data/getJoueuse/${id}`, { withCredentials: true })
+            .then((res) => {
+                console.log("datas", res.data);
+                setDatas(res.data);
+            })
+            .catch((err) => {
+                console.error("Erreur lors de la récupération des données :", err);
+            });
+
     }, [id]);
 
     console.log("Profil de la joueuse avec l'ID :", id);
 
-    return (
-        <div>
-            ProfilJoueuse {joueuse ? joueuse.nom : "Chargement..."}
-            {joueuse && (
-                <CarteJoueuse joueuse={joueuse.nom} />
-            )}
-        </div>
-    );
+ return (
+    <div className="space-y-6">
+      <h1 className="text-2xl font-bold">
+        Profil Joueuse {joueuse ? joueuse.nom : "Chargement..."}
+      </h1>
+      
+      {joueuse && datas && (
+        <>
+          <CarteJoueuse joueuse={joueuse.nom} />
+          
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <GraphStats 
+              stats={datas.tauxTirsReussis} 
+              titre="Taux de tirs réussis" 
+            />
+            
+            <GraphStats 
+              stats={datas.tirsReussis} 
+              titre="Tirs réussis" 
+            />
+            
+            <GraphStats 
+              stats={datas.tirsTotal} 
+              titre="Tirs total" 
+            />
+            
+            <GraphStats 
+              stats={datas.passeD} 
+              titre="Passes décisives" 
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ProfilJoueuse;
