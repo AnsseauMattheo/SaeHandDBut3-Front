d916ab42b9b6dbbf2525307dd5c95082df57177a
Skyforrr <remy.leconte@uphf.fr>
2025-11-20 11:46:38 +0100

    fix pb

--- src/axiosRequests.js
+++ src/axiosRequests.js
@@ -33,3 +33,49 @@ export async function getLastMatch() {
         return false;
     }
 }
+
+export async function getInsights() {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/match/insights`, {
+            withCredentials: true
+        });
+
+        return response.data;
+
+    } catch (error) {
+        console.error("Erreur lors de la récupération des insights:", error);
+        // Retourne un tableau vide en cas d'erreur pour éviter les crashs
+        return [];
+    }
+}
+
+
+export const getPossessionsByPhase = async () => {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/match/possessionsByPhase`);
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des possessions par phase:", error);
+        throw error;
+    }
+};
+
+export const getZonesRanking = async () => {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/match/zonesRanking`);
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors de la récupération du classement des zones:", error);
+        return [];
+    }
+};
+
+export const getTopPlayers = async () => {
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/match/topPlayers`);
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors de la récupération du top joueuses:", error);
+        return [];
+    }
+};
