c6876016e3540e8a2de35db7cbed11b1fda37613
adam.sautiere <adam.sautiere@uphf.fr>
2025-11-21 11:24:33 +0100

    correction carte fut gardienne + changement de tirs pour arrets gardienne

--- src/Pages/ProfilJoueuse.jsx
+++ src/Pages/ProfilJoueuse.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { Car } from "lucide-react";
-import { use, useEffect, useState } from "react";
-import { useParams } from "react-router";
+import {Car} from "lucide-react";
+import {use, useEffect, useState} from "react";
+import {useParams} from "react-router";
 import CarteJoueuse from "../components/CarteJoueuse";
 import GraphStats from "../components/GraphStats";
 import RadarStats from "@/components/RadarStats.jsx";
@@ -9,109 +9,111 @@ import GraphStatsDef from "../components/GraphStatsDef";
 
 const ProfilJoueuse = () => {
 
-  const { id } = useParams();
-  const [joueuse, setJoueuse] = useState(null);
-  const [datas, setDatas] = useState(null);
-  const [evalueData, setEvalueData] = useState(null);
-  const [defStats, setDefStats] = useState(null);
+    const {id} = useParams();
+    const [joueuse, setJoueuse] = useState(null);
+    const [datas, setDatas] = useState(null);
+    const [evalueData, setEvalueData] = useState(null);
+    const [defStats, setDefStats] = useState(null);
 
-  useEffect(() => {
-    axios.get(`${import.meta.env.VITE_SERVER_URL}/joueuses/joueuse/${id}`, { withCredentials: true })
-      .then((res) => {
-        setJoueuse(res.data);
-      })
-      .catch((err) => {
-        console.error("Erreur lors de la récupération du profil :", err);
-      });
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_SERVER_URL}/joueuses/joueuse/${id}`, {withCredentials: true})
+            .then((res) => {
+                setJoueuse(res.data);
+            })
+            .catch((err) => {
+                console.error("Erreur lors de la récupération du profil :", err);
+            });
 
-    axios.get(`${import.meta.env.VITE_SERVER_URL}/data/getJoueuse/${id}`, { withCredentials: true })
-      .then((res) => {
-        setDatas(res.data);
-      })
-      .catch((err) => {
-        console.error("Erreur lors de la récupération des données :", err);
-      });
+        axios.get(`${import.meta.env.VITE_SERVER_URL}/data/getJoueuse/${id}`, {withCredentials: true})
+            .then((res) => {
+                setDatas(res.data);
+            })
+            .catch((err) => {
+                console.error("Erreur lors de la récupération des données :", err);
+            });
 
-    axios.get(`${import.meta.env.VITE_SERVER_URL}/joueuses/dataParAffectation`, { withCredentials: true })
-      .then((res) => {
-        setEvalueData(res.data);
-      })
-      .catch((err) => {
-        console.error("Erreur lors de la récupération des données par affectation :", err);
-      });
+        axios.get(`${import.meta.env.VITE_SERVER_URL}/joueuses/dataParAffectation`, {withCredentials: true})
+            .then((res) => {
+                setEvalueData(res.data);
+            })
+            .catch((err) => {
+                console.error("Erreur lors de la récupération des données par affectation :", err);
+            });
 
-    axios.get(`${import.meta.env.VITE_SERVER_URL}/data/getJoueuseDef/${id}`, { withCredentials: true })
-      .then((res) => {
-        setDefStats(res.data);
-      })
-      .catch((err) => {
-        console.error("Erreur lors de la récupération des données défensives :", err);
-      });
+        axios.get(`${import.meta.env.VITE_SERVER_URL}/data/getJoueuseDef/${id}`, {withCredentials: true})
+            .then((res) => {
+                setDefStats(res.data);
+            })
+            .catch((err) => {
+                console.error("Erreur lors de la récupération des données défensives :", err);
+            });
 
-  }, [id]);
+    }, [id]);
 
 
-  return (
-    <div className="space-y-6">
-      <h1 className="text-2xl font-bold">
-        Profil Joueuse {joueuse ? joueuse.nom : "Chargement..."}
-      </h1>
+    return (
+        <div className="space-y-6">
+            <h1 className="text-2xl font-bold">
+                Profil Joueuse {joueuse ? joueuse.nom : "Chargement..."}
+            </h1>
 
-      {joueuse && datas && defStats && (
-        <>
-          <div className="flex flex-col lg:flex-row gap-6 w-full h-80]">
-            <div className="w-full lg:w-1/4 ">
-              <CarteJoueuse joueuse={joueuse.nom} affectation={joueuse.affectation.affectation} />
-            </div>
-            <div className="w-full lg:w-3/4 ">
-              <RadarStats joueuse={joueuse.nom} evalueData={evalueData} affectation={joueuse.affectation.affectation} />
-            </div>
-          </div>
+            {joueuse && datas && defStats && (
+                <>
+                    <div className="flex flex-col lg:flex-row gap-6 w-full h-80]">
+                        <div className="w-full lg:w-1/4 ">
+                            <CarteJoueuse joueuse={joueuse.nom} affectation={joueuse.affectation.affectation}/>
+                        </div>
+                        <div className="w-full lg:w-3/4 ">
+                            <RadarStats joueuse={joueuse.nom} evalueData={evalueData}
+                                        affectation={joueuse.affectation.affectation}/>
+                        </div>
+                    </div>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-            <GraphStats
-              stats={datas.tauxTirsReussis}
-              titre="Taux de tirs réussis"
-            />
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
 
-            <GraphStats
-              stats={datas.tirsReussis}
-              titre="Tirs réussis"
-            />
+                        <GraphStats
+                            stats={datas.tirsTotal}
+                            titre={joueuse.affectation.affectation.affectation === "Gardienne" ? "Taux de tirs réussis" : "Taux de arrets réussis"}
+                        />
 
-            <GraphStats
-              stats={datas.tirsTotal}
-              titre="Tirs total"
-            />
+                        <GraphStats
+                            stats={datas.tirsTotal}
+                            titre={joueuse.affectation.affectation.affectation === "Gardienne" ? "Tirs réussis" : "Arrets réussis"}
+                        />
 
-            <GraphStats
-              stats={datas.passeD}
-              titre="Passes décisives"
-            />
-            <GraphStats
-              stats={datas.perteBalle}
-              titre="Pertes de balle"
-            />
+                        <GraphStats
+                            stats={datas.tirsTotal}
+                            titre={joueuse.affectation.affectation.affectation === "Gardienne" ? "Tirs total" : "Arrets total"}
+                        />
 
-              {joueuse.affectation.affectation.affectation === "Gardienne" ?
-            <GraphStatsDef
-              defStats={defStats.defPlus}
-              titre="Statistiques Défensives Plus"
-            />
-                  : ""}
+                        <GraphStats
+                            stats={datas.passeD}
+                            titre="Passes décisives"
+                        />
+                        <GraphStats
+                            stats={datas.perteBalle}
+                            titre="Pertes de balle"
+                        />
 
-              {joueuse.affectation.affectation.affectation === "Gardienne" ?
-            <GraphStatsDef
-              defStats={defStats.defMoins}
-              titre="Statistiques Défensives Moins"
-            />
-                  : ""}
+                        {joueuse.affectation.affectation.affectation === "Gardienne" ?
+                            <GraphStatsDef
+                                defStats={defStats.defPlus}
+                                titre="Statistiques Défensives Plus"
+                            />
+                            : ""}
 
-          </div>
-        </>
-      )}
-    </div>
-  );
+                        {joueuse.affectation.affectation.affectation === "Gardienne" ?
+                            <GraphStatsDef
+                                defStats={defStats.defMoins}
+                                titre="Statistiques Défensives Moins"
+                            />
+                            : ""}
+
+                    </div>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default ProfilJoueuse;
